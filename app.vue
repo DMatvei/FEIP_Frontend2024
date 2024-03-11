0fa8ba3d0427c8e265bb839f487c98e66fffc109
DMatvei <matvei.dranitsyn@yandex.ru>
2024-03-12 01:50:22 +1000

    Исправление ошибок первого задания, переход на новую версию

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="title">Hellow World! 1</h1>
+    <p> Hello world 2</p>
+  </div>
+  
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+  :root {
+    font-family: "Open Sans", sans-serif;
+  }
+
+  .title {
+    font-family: "Montserrat", sans-serif;
+  }
+</style>
